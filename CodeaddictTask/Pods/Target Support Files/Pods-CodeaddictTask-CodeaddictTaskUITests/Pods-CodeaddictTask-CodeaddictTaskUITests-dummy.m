1bcacb7e7f99949d766ac8c97277061701d7250f
Tymoteusz Sala <tymek.pawel@gmail.com>
2020-12-12 20:12:57 +0100

    Added necessary pods: SwiftyJSON, RxSwift, RxCocoa

---
+++ CodeaddictTask/Pods/Target Support Files/Pods-CodeaddictTask-CodeaddictTaskUITests/Pods-CodeaddictTask-CodeaddictTaskUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodeaddictTask_CodeaddictTaskUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CodeaddictTask_CodeaddictTaskUITests
+@end
