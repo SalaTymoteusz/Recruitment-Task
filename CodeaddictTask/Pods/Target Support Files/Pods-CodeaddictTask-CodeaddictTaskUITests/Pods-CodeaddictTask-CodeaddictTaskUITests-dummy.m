97096c0b4d765c1644a4caa3ec9c989778b40398
Tymoteusz Sala <tymek.pawel@gmail.com>
2020-12-14 20:24:46 +0100

    Delete unnecessary pods

--- CodeaddictTask/Pods/Target Support Files/Pods-CodeaddictTask-CodeaddictTaskUITests/Pods-CodeaddictTask-CodeaddictTaskUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CodeaddictTask_CodeaddictTaskUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CodeaddictTask_CodeaddictTaskUITests
-@end
